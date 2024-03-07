35226c13660f38533adb25e36f3fb071625bfbc9
Omar <saucyfunnel@gmail.com>
2024-03-07 18:13:45 -0500

    database wrk

--- database/migrations/2024_03_07_225209_create_posts_table.php
+++ database/migrations/2024_03_07_225209_create_posts_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->longText('body')->nullable();
+            $table->foreignId('user_id')->constrained('user');
+            $table->foreignId('deleted_by')->nullable()->constrained('users');
+            $table->timestamp('deleted_at')->nullable();
             $table->timestamps();
         });
     }
