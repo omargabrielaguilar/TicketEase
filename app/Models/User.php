c5a835663327bf19ebf114f09cfae1d1fde52fd7
Omar <saucyfunnel@gmail.com>
2025-01-25 15:48:44 -0500

    php cs fixer

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,18 +7,17 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable   
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+            use HasFactory, Notifiable; 
 
     /**
      * The attributes that are mass assignable.
      *
      * @var list<string>
      */
-    protected $fillable = [
-        'name',
+    protected $fillable = [       'name',
         'email',
         'password',
     ];
@@ -45,5 +44,4 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
-
 }
