43598650d1196cc99ddce8ae07f69ab49e8bd4fe
Omar <saucyfunnel@gmail.com>
2025-01-26 18:22:10 -0500

    add testify and socket

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,17 +7,19 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable   
+class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-            use HasFactory, Notifiable; 
+    use HasFactory;
+    use Notifiable;
 
     /**
      * The attributes that are mass assignable.
      *
      * @var list<string>
      */
-    protected $fillable = [       'name',
+    protected $fillable = [
+        'name',
         'email',
         'password',
     ];
